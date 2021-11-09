96a02730981eb44ecdfcb8e91694ba4d94fce10c
SNEHABHIxd <93216312+SNEHABHIxd@users.noreply.github.com>
2021-11-09 18:10:56 +0530

    Create voice_chat_ended.py

---
+++ snehabhiXmusic/snehuxabhi/voice_chat_ended.py
@@ -0,0 +1,19 @@
+from pyrogram import Client, filters
+
+from pyrogram.types import Message
+
+from snehabhiXmusic.abhi.channelmusic import get_chat_id
+
+from snehabhiXmusic.kingXqueen.callsmusic.callsmusic import remove
+
+@Client.on_message(filters.voice_chat_ended)
+
+async def voice_chat_ended(_, message: Message):
+
+    try:
+
+        remove(get_chat_id(message.chat))
+
+    except Exception:
+
+        pass
