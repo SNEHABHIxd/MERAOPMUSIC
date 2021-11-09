b76ebf1d302689111f9d6afa0650ada687b10c58
SNEHABHIxd <93216312+SNEHABHIxd@users.noreply.github.com>
2021-11-09 20:14:31 +0530

    Update __main__.py

--- snehabhiXmusic/__main__.py
+++ snehabhiXmusic/__main__.py
@@ -4,7 +4,7 @@ from pyrogram import Client as Bot
 
 from snehabhiXmusic.config import API_HASH, API_ID, BG_IMAGE, BOT_TOKEN
 
-from snehabhiXmusic.kingXqueen.callsmusic import run
+from snehabhiXmusic.services.callsmusic import run
 
 response = requests.get(BG_IMAGE)
 
