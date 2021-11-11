36aa28e058829aff2d046d59f146244069919975
SNEHABHIxd <93216312+SNEHABHIxd@users.noreply.github.com>
2021-11-11 19:33:04 +0530

    Update channelplay.py

--- snehabhiXmusic/snehuxabhi/channelplay.py
+++ snehabhiXmusic/snehuxabhi/channelplay.py
@@ -737,8 +737,8 @@ async def play(_, message: Message):
         if message.reply_to_message:
 
             text = message.reply_to_message.text \
-
                 or message.reply_to_message.caption
+                
 
             if message.reply_to_message.entities:
 
