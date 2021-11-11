e53556a9c4c9c5b4ffa1c88d86b436647e9ffb8f
SNEHABHIxd <93216312+SNEHABHIxd@users.noreply.github.com>
2021-11-11 21:00:48 +0530

    Update song.py

--- snehabhiXmusic/snehuxabhi/song.py
+++ snehabhiXmusic/snehuxabhi/song.py
@@ -34,7 +34,7 @@ from youtubesearchpython import SearchVideos
 
 from snehabhiXmusic.config import DURATION_LIMIT
 
-from snehabhiXmusic.snehaxabhi.play import arq
+#KANGERS KI MAA KA BHOSDA
 
 @Client.on_message(filters.command("song") & ~filters.channel)
 
