fe568a9b52e518e996ea90b7983686367278db95
SNEHABHIxd <93216312+SNEHABHIxd@users.noreply.github.com>
2021-11-12 10:31:04 +0530

    Update admins.py

--- snehabhiXmusic/abhi/admins.py
+++ snehabhiXmusic/abhi/admins.py
@@ -1,31 +1,18 @@
 from typing import List
 
-from pyrogram.types import Chat
+from pyrogram.types import Chat, User
 
-from snehabhiXmusic.Snehu.admins import get as gett
+from snehabhiXmusic.abhi.admins import get as gett
+from snehabhiXmusic.abhi.admins import set
 
-from snehabhiXmusic.Snehu.admins import set
 
-async def get_administrators(chat: Chat) -> List[int]:
-
+async def get_administrators(chat: Chat) -> List[User]:
     get = gett(chat.id)
 
     if get:
-
         return get
+    administrators = await chat.get_members(filter="administrators")
+    to_set = [administrator.user.id for administrator in administrators]
 
-    else:
-
-        administrators = await chat.get_members(filter="administrators")
-
-        to_set = []
-
-        for administrator in administrators:
-
-            if administrator.can_manage_voice_chats:
-
-                to_set.append(administrator.user.id)
-
-        set(chat.id, to_set)
-
-        return await get_administrators(chat)
+    set(chat.id, to_set)
+    return await get_administrators(chat)
