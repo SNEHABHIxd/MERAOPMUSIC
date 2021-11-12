c272e5a3e0bc7f1b26068b7ea88757ab77477844
SNEHABHIxd <93216312+SNEHABHIxd@users.noreply.github.com>
2021-11-12 12:12:44 +0530

    Update __init__.py

--- snehabhiXmusic/services/callsmusic/__init__.py
+++ snehabhiXmusic/services/callsmusic/__init__.py
@@ -1,8 +1,2 @@
-from pyrogram import Client
-
-from snehabhiXmusic import config
-
-client = Client(config.SESSION_NAME, config.API_ID, config.API_HASH)
-
-run = client.run
-
+from snehabhiXmusic.services.callsmusic import pytgcalls, run
+from . import snehabhiXmusic.kingXqueen.queues
